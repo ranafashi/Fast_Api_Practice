194af1f66b5f1cde6501e63c4b03dfe7ea487ead
Hamad Jamil <ranafashi789@gmail.com>
2026-07-29 09:44:38 +0500

    Database Configuration

--- db_config.py
+++ db_config.py
@@ -3,7 +3,7 @@ from config import settings
 
 client = MongoClient(settings.mongodb_url)
 db = client[settings.database_name]
-collection = db["Products"]  # produts
+collection = db["Products"]  
 user_collection = db["Users"]
 carts =db["Carts"]
 order_collection = db["Orders"]
